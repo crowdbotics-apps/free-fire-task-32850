d66b73b9f9ef7f5d7897418b4b1307dbaec9bf83
Crowdbotics <team@crowdbotics.com>
2022-01-15 02:45:02 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/free_fire_task_32850/MainActivity.java
@@ -0,0 +1,15 @@
+package com.free_fire_task_32850;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "free_fire_task_32850";
+  }
+}
